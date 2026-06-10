19ff728492ca5b57d3cb8cca7b5488de67d5de2f
dfggfdffdgfg <deborasevero313@gmail.com>
2026-06-10 13:41:22 -0300

    novas alterções

--- src/controllers/VeiculoController.js
+++ src/controllers/VeiculoController.js
@@ -1,27 +1,27 @@
 class VeiculoController {
-    constructor(service) {  //RECEBE O SERVIÇO
-        this.service = service;
+    constructor(service) {
+        this.service = service; // recebe serviço
     }
 
     async index(req, res) {
         const veiculos = await this.service.listarTodos();
-        res.json(veiculos); //LISTA DE VEICULOS
+        res.json(veiculos); // listação de veiculos
     }
 
     async store(req, res) {
         try {
             const veiculo = await this.service.registrarEntrada(req.body);
-            res.status(201).json(veiculo);//RETORNA AO VEICULO CRIADO
+            res.status(201).json(veiculo); // retorna para o veiculo criado
         } catch (error) {
-            res.status(400).json({ erro: error.message });
+            res.status(400).json({ erro: error.mensagem });
         }
     }
 
     async release(req, res) {
         try {
             const { vaga } = req.params;
-            await this.service.registrarSaida(vaga);//LIBERAÇÃO DE VAGA
-            res.json({ mensagem: "Vaga liberada!" });
+            await this.service.registrarSaida(vaga); // liberar vaga
+                res.json({ mensagem: "Vaga liberada!" });            
         } catch (error) {
             res.status(400).json({ erro: error.message });
         }
